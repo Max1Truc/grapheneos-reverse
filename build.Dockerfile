cdc1cbbff247d4d11529c4fbbace54cdda78f80d
Max1Truc <max1truc@disroot.org>
2026-03-05 00:23:48 +0100

    update docker stuff

---
+++ build.Dockerfile
@@ -0,0 +1,14 @@
+FROM archlinux:latest
+
+ENV TZ="America/New_York"
+RUN useradd -ms /bin/bash reprobuilder && \
+    pacman -Syyuu --noconfirm base base-devel jq cpio diffutils fontconfig freetype2 git gnupg inetutils make nodejs-lts-krypton openssh openssl parallel python-protobuf python repo rsync signify ttf-dejavu zip unzip yarn gperf lib32-gcc-libs lib32-glibc && \
+    mkdir -pv /opt/build/grapheneos/ && chown -R reprobuilder:reprobuilder /opt/build/ && \
+    ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime;
+
+COPY ./*.sh /usr/local/bin
+RUN chmod a+rx /usr/local/bin/*.sh
+
+WORKDIR /opt/build/grapheneos
+
+USER reprobuilder
---
+++ fetch_source.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ -z "${GOS_BUILD_NUMBER}" ]
+then
+	echo missing env GOS_BUILD_NUMBER
+	exit 1
+fi
+
+# Initial builder preparation.
+export OFFICIAL_BUILD=true
+mkdir -pv ~/.ssh
+curl -sL https://grapheneos.org/allowed_signers > ~/.ssh/grapheneos_allowed_signers
+git config --global user.name "grapheneos"
+git config --global user.email "grapheneos-build@localhost"
+git config --global color.ui false
+if [[ -f "/.gitcookies" ]]; then
+  git config --global http.cookiefile /.gitcookies
+fi
+
+# Fetch OS source code tree.
+echo "[INFO] Fetching OS tree..."
+mkdir -p "/opt/build/grapheneos/grapheneos-${GOS_BUILD_NUMBER}"
+cd "/opt/build/grapheneos/grapheneos-${GOS_BUILD_NUMBER}"
+repo init --depth=1 -u https://github.com/GrapheneOS/platform_manifest.git -b "refs/tags/${GOS_BUILD_NUMBER}"
+cd .repo/manifests
+git config gpg.ssh.allowedSignersFile ~/.ssh/grapheneos_allowed_signers
+git verify-tag $(git describe)
+cd ../..
+repo sync -j8 --retry-fetches=6 --force-sync --no-clone-bundle --no-tags
