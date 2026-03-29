a3ad69493885c335fd38d4b0e2cef77cd9336809
Max1Truc <max1truc@disroot.org>
2026-03-29 12:19:26 +0200

    add a script to quickly rebuild

---
+++ rebuild_source.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -exo pipefail
+
+export BUILD_NUMBER="$1"
+export BUILD_DATETIME="$2"
+export OFFICIAL_BUILD=true
+
+if [ -z "${BUILD_NUMBER}" -o -z "${BUILD_DATETIME}" ]
+then
+	echo missing parameters BUILD_NUMBER and BUILD_DATETIME
+	exit 1
+fi
+
+echo '[..] BUILDING'
+
+source build/envsetup.sh
+yarn --cwd vendor/adevtool/ install
+lunch tegu-cur-user
+m
+
+echo '[OK] BUILD SUCCESS'
